9e51e1673fd196762ae95664466af03c33962fdb
Ahmed AbuMusa <ahmedabumusa@outlook.com>
2024-10-01 02:14:52 +0300

    Java packaged

---
+++ src/main/java/abumusa/org/App.java
@@ -0,0 +1,15 @@
+package abumusa.org;
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+        Logger logger = Logger.getLogger("MyLogger");
+        logger.info("My Message");
+    }
+}
